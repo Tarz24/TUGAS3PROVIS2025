dde50adff026d883ff5b8e7617f697bc2b11376e
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-06-19 01:17:25 +0700

    sebuah file untuk memudahkan import ke dalam tiap file yang menggunakan logika supabase

---
+++ tugas_provis/lib/supabase_client.dart
@@ -0,0 +1,7 @@
+// lib/supabase_client.dart
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+// Definisikan instance client Supabase di sini sebagai variabel global
+// yang bisa diakses dari mana saja setelah di-import.
+final supabase = Supabase.instance.client;
